9de23e56adb7802ae80e3da0e866b455c303b3a9
OGWENO4 <hildebertsleonard@gmail.com>
2026-09-12 03:04:27 -0700

    project in json

---
+++ script.js
@@ -0,0 +1,23 @@
+const student = {
+    name:"Hildeberts Leonard",
+    age:16,
+    country:"Kenya",
+    course:"JSON & JavaScript",
+    subjects:[
+        "Mathematics",
+        "English",
+        "Computer Studies"
+],
+    skills:[
+        "HTML",
+        "CSS",
+        "JavaScript"
+    ]
+};
+
+console.log("Name:", student.name );
+console.log("Age:", student.age);
+console.log("Country:", student.country);
+console.log("Course:", student.course);
+console.log("skills:", student.skills);
+console.log("subject:", student.subjects);
